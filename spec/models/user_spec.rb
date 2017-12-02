d86f767bbaef7e6022db98b9a7bd820c3b826164
Franzé Jr <franzejr@gmail.com>
2017-12-02 16:13:43 -0300

    Fix small details after revision

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,5 +1,14 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe 'fields' do
+    it { is_expected.to respond_to(:email) }
+    it { is_expected.to respond_to(:password) }
+    it { is_expected.to respond_to(:uid) }
+    it { is_expected.to respond_to(:provider) }
+  end
+
+  describe 'factory is ok' do
+    it { FactoryGirl.create(:user) }
+  end
 end
