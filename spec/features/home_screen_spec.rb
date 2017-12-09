279b6cb13a1fd69c1d30bc9c6afa64eba2f37267
Franzé Jr <franzejr@gmail.com>
2017-12-09 16:03:03 -0300

    Fix tests

--- spec/features/home_screen_spec.rb
+++ spec/features/home_screen_spec.rb
@@ -3,8 +3,11 @@ require 'rails_helper'
 RSpec.feature 'Home Screen', type: :feature do
   let!(:ruby) { create(:checklist, title: 'Ruby') }
   let!(:elm) { create(:checklist, title: 'Elm') }
+  let(:user) { create(:user) }
 
   scenario 'displays checklists' do
+    visit checklist_path(ruby)
+    log_in(user)
     visit root_path
 
     expect(page).to have_checklist_link('Ruby')
--- spec/mailers/share_checklist_spec.rb
+++ spec/mailers/share_checklist_spec.rb
@@ -2,16 +2,19 @@ require 'rails_helper'
 
 RSpec.describe ShareChecklistMailer, type: :mailer do
   describe 'email' do
-    let(:mail) { ShareChecklistMailer.email }
+    let(:from_user) { double('User', email: 'from@example.com') }
+    let(:to_user) { double('User', email: 'to@example.org') }
+    let(:checklist) { double('Checklist', id: 1) }
+    let(:mail) { ShareChecklistMailer.email(from_user, to_user, checklist) }
 
     it 'renders the headers' do
-      expect(mail.subject).to eq('Email')
+      expect(mail.subject).to eq("#{from_user.email} has shared a Checklist with you!")
       expect(mail.to).to eq(['to@example.org'])
-      expect(mail.from).to eq(['from@example.com'])
+      expect(mail.from).to eq([ENV['PRODUCITON_EMAIL']])
     end
 
     it 'renders the body' do
-      expect(mail.body.encoded).to match('Hi')
+      expect(mail.body.encoded).to include('Howdy')
     end
   end
 end
