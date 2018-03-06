045fe9a626d3ed6c5e2000afac979a6c8ac2b6fa
Franzé Jr <franzejr@gmail.com>
2018-03-06 19:15:00 -0300

    Fix broken tests

--- spec/mailers/share_checklist_spec.rb
+++ spec/mailers/share_checklist_spec.rb
@@ -2,15 +2,21 @@ require 'rails_helper'
 
 RSpec.describe ShareChecklistMailer, type: :mailer do
   describe 'email' do
-    let(:from_user) { double('User', email: 'from@example.com') }
-    let(:to_user) { double('User', email: 'to@example.org') }
+    let(:from_user) { double('User', email: 'from@example.com', id: 1) }
+    let(:to_user) { double('User', email: 'to@example.org', id: 2) }
     let(:checklist) { double('Checklist', id: 1) }
-    let(:mail) { ShareChecklistMailer.email(from_user, to_user, checklist) }
+    let(:mail) { ShareChecklistMailer.email(from_user.id, to_user.id, checklist.id) }
+    before do
+      expect(User).to receive(:find).with(1).and_return(from_user)
+      expect(User).to receive(:find).with(2).and_return(to_user)
+      expect(Checklist).to receive(:find).with(1).and_return(checklist)
+    end
 
    it 'renders the headers' do
       expect(mail.subject).to eq("#{from_user.email} has shared a Checklist with you!")
       expect(mail.to).to eq(['to@example.org'])
-      expect(mail.from).to eq([ENV['PRODUCITON_EMAIL']])
+      prod_email = ENV['PRODUCITON_EMAIL'] ? [ENV['PRODUCITON_EMAIL']] : []
+      expect(mail.from).to eq(prod_email)
     end
 
     it 'renders the body' do
